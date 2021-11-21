7b0389273e6abd88b485f70b26ed35102855e28d
Raúl Soto <sotosanchezraul@gmail.com>
2021-11-21 21:36:54 +0100

    Red IsUndoable

---
+++ src/main/java/designpatterns/connect4/models/Registry.java
@@ -0,0 +1,8 @@
+package designpatterns.connect4.models;
+
+public class Registry {
+
+    public boolean isUndoable(){
+        return true;
+    }
+}
---
+++ src/test/java/designpatterns/connect4/models/RegistryTest.java
@@ -0,0 +1,21 @@
+package designpatterns.connect4.models;
+
+import org.junit.jupiter.api.Test;
+
+import designpatterns.connect4.types.Color;
+import designpatterns.utils.models.ConcreteCoordinate;
+import designpatterns.utils.models.Direction;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class RegistryTest {
+
+    @Test
+    public void givenNewRegistryWhenIsUndoableThenFalse(){
+        assertThat(new Registry().isUndoable(), is(false));
+    }
+}
